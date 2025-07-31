a92af8308521974743477ee999d166a7dcb972bf
Abhinav Mishra <abhinavmishra12514@gmail.com>
2025-07-31 10:13:24 +0530

    Eighteen Commit

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = "http://localhost:3000"; // Your backend URL
+const URL = "https://vercel-backend-test-five.vercel.app"; // Your backend URL
 export const socket = io(URL, {
     autoConnect: false // We will connect manually
 });
